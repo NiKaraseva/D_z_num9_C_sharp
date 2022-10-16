eeef7f27f68cc3ad54d8e48fad5ed307591181c5
Nika <nikarasevas@gmail.com>
2022-10-16 17:20:37 +0300

    Рекурсия: вывели все натуральные числа от n до 1

---
+++ Exercise_01/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */ 
+
+Console.WriteLine("Введите значение n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+string NumRecur(int n, int min)
+{
+    if(n == min)
+    {
+        return n.ToString();
+    }
+    else
+    {
+        return($"{n}, "+ NumRecur(n - 1, min));
+    }
+}
+
+Console.WriteLine(NumRecur(n, 1));
