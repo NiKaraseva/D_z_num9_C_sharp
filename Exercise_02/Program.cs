524b00cadf1e1097cdec43ea5d5a54f724e19732
Nika <nikarasevas@gmail.com>
2022-10-16 17:22:32 +0300

    Рекурсия: сумма элементов от m до n

---
+++ Exercise_02/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */ 
+
+Console.WriteLine("Введите значение m: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int SumRecur(int m, int n)
+{
+    if(m == n)
+    {
+        return m;
+    }
+    else
+    {
+        return(n + SumRecur(m, n - 1));
+    }
+}
+
+Console.WriteLine(SumRecur(m, n));
